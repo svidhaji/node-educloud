0dda740c04a211f14906ccea216983582961af41
Said Haji <svidhaji@gmail.com>
2019-04-18 10:27:33 +0300

    removed ignored files

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const mysql = require('mysql2');
 
+//Creating connection to database
 const conn = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -15,20 +16,16 @@ const conn = mysql.createConnection({
 
 app.get('/', (req, res) => {
   conn.query(
-  'CREATE TABLE snake',
-	(err, results, fields) => {
-     console.log(results);
-     console.log(fields);
-}
+      'CREATE TABLE snake',
+      (err, results, fields) => {
+        console.log(results);
+        console.log(fields);
+      })
 });
 
 app.post('/', bodyParser.urlencoded({extended : true}),(req, res) => {
   console.log(req.body);
   res.send('POst: HEllo' + req.body.name);
-});
-app.post('/', bodyParser.urlencoded({extended : true}),(req, res) => {
-  console.log(req.query);
-  res.send('POst: HEllo' + req.query.name);
 });
 
 app.listen(3000);
